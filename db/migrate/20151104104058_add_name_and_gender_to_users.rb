0ab0f69f14a933ffcb4420e5ca6e97b3fd284c00
nozayasu <asid961@yahoo.co.jp>
2015-11-04 19:42:44 +0900

    Add name and gender to users

---
+++ db/migrate/20151104104058_add_name_and_gender_to_users.rb
@@ -0,0 +1,6 @@
+class AddNameAndGenderToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :gender, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150813013210) do
+ActiveRecord::Schema.define(version: 20151104104058) do
 
   create_table "authentications", force: :cascade do |t|
     t.integer  "user_id",    limit: 4,   null: false
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20150813013210) do
     t.string   "salt",             limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name",             limit: 255
+    t.string   "gender",           limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
